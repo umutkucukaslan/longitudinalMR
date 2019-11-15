88f126873fe8519f91dc1604cb68fbe590cb6eed
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-15 19:11:49 +0100

    colab integration test

---
+++ dataset.py
@@ -0,0 +1,5 @@
+
+
+def get_train_dataset():
+    train_x, train_y = ([1, 2, 3, 4], [10, 20, 50, 80])
+    return (train_x, train_y)
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
-print(tf.__version__)
+from dataset import get_train_dataset
 
+
+
+def main():
+    train_x, train_y = get_train_dataset()
+    print("train_x is   ", train_x)
+    print("train_y is   ", train_y)
