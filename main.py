9f34f30a0f99d8fbe2527582778aefde2836aa7d
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-17 22:02:11 +0100

    colab integration test

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from dataset import get_train_dataset
 
 def main():
     train_x, train_y = get_train_dataset()
-    print("train_x is   ", train_x)
+    print("train_x is   ", train_x*10000)
     print("train_y is   ", train_y)
