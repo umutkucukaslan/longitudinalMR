18bccf18514047a328217b882c72d0333eb77af0
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-17 22:08:25 +0100

    colab integration test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from dataset import get_train_dataset
-
+from tensorflow.python.client import device_lib
 
 
 def main():
@@ -8,5 +8,7 @@ def main():
     print("train_x is   ", train_x*10000)
     print("train_y is   ", train_y)
     print("Hello world")
+    print("DEVICES")
+    print(device_lib.list_local_devices())
 
 main()
