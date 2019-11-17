19c64d081f3cb5eb39911f832a76029e9d7d8e65
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-18 00:01:53 +0100

    colab integration test

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ mnist = tf.keras.datasets.mnist
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test, verbose=2)
