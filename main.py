40aa9f44977151f7166b62a6b62b515351f5819d
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-25 16:01:44 +0100

    test colab integration

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ from dataset import Dataset
 x = tf.random.uniform([3, 3])
 
 
-print("Is there a GPU available: "),
-print(tf.config.experimental.list_physical_devices("GPU"))
-
-
-print("Is the Tensor on GPU #0:  "),
-print(x.device.endswith('GPU:0'))
+print(x.device)
 
 
 
