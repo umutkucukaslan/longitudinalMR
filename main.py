cc293f354ac456a3e2476007e8200edee46892a8
umutkucukaslan <umutkucukaslan@hotmail.com>
2019-11-25 15:05:06 +0100

    test colab integration

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import tensorflow as tf
+from dataset import Dataset
+
+dataset = Dataset()
 
 
 
-mnist = tf.keras.datasets.mnist
 
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-print('labels')
-print(y_train)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
 
-# model.fit(x_train, y_train, epochs=5)
-# model.evaluate(x_test, y_test, verbose=2)
+
+# mnist = tf.keras.datasets.mnist
+#
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# print('labels')
+# print(y_train)
+#
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28, 28)),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(10, activation='softmax')
+# ])
+#
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+# # model.fit(x_train, y_train, epochs=5)
+# # model.evaluate(x_test, y_test, verbose=2)
