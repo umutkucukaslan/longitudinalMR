9d7e0813185c0f1a2a7fe902a6192831104fa679
umutkucukaslan <umutkucukaslan@hotmail.com>
2020-01-13 22:32:02 +0100

    test on google colab

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
+import configparser
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from dataset import Dataset
+import numpy as np
+import logging
 
-# dataset = Dataset()
-
-x = tf.random.uniform([3, 3])
-
-
-print(x.device)
+from dataset import get_autoencoder_dataset
 
 
+config = configparser.ConfigParser()
+config.read("./config.ini")
+
+logging.root.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler(config['Logging'].get('log_file_pth'))
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+logger.info('merhaba')
+
+
+tf.enable_eager_execution()
+
+
+def show_batch(image_batch):
+    plt.figure(figsize=(30, 30))
+    for n in range(25):
+        ax = plt.subplot(5, 5, n + 1)
+        plt.imshow(np.squeeze(image_batch[n]), cmap='gray')
+        plt.axis('off')
+    plt.show()
+
+
+train_ds, val_ds, test_ds = get_autoencoder_dataset()
+logger.info('Dataset for autoencoder created.')
+image_batch, label_batch = next(iter(train_ds))
+show_batch(image_batch=image_batch)
+show_batch(image_batch=label_batch)
 
 
 
