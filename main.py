b13339bcb1aa9176afcd00fe23dee9efdec4e51d
umutkucukaslan <umutkucukaslan@hotmail.com>
2020-01-19 10:49:36 +0100

    model definition and training

--- main.py
+++ main.py
@@ -1,72 +1,51 @@
 import configparser
+import sys
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-import logging
 
-from dataset import get_autoencoder_dataset
-
-
-config = configparser.ConfigParser()
-config.read("./config.ini")
-
-logging.root.setLevel(logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-# Create handlers
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler(config['Logging'].get('log_file_pth'))
-c_handler.setLevel(logging.DEBUG)
-f_handler.setLevel(logging.DEBUG)
-
-# Create formatters and add it to handlers
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-
-logger.info('merhaba')
+from dataset import get_autoencoder_dataset, get_fake_autoencoder_dataset
+from model.autoencoder import build_encoder, build_decoder
+from setup_logging import logger
 
 
 tf.enable_eager_execution()
 
-
-def show_batch(image_batch):
-    plt.figure(figsize=(10, 10))
-    for n in range(25):
-        ax = plt.subplot(5, 5, n + 1)
-        plt.imshow(np.squeeze(image_batch[n]), cmap='gray')
-        plt.axis('off')
-    plt.show()
-
 train_ds, val_ds, test_ds = get_autoencoder_dataset()
-logger.info('Dataset for autoencoder created.')
-image_batch, label_batch = next(iter(train_ds))
-show_batch(image_batch=image_batch)
-show_batch(image_batch=label_batch)
+
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+batch_size = 32
+input_shape = (256, 256, 1)
+latent_size = 32
+filters = (8, 16, 32)
+kernel_size = 3
+pool_size = (2, 2)
+batch_normalization = False
+log_dir = config['Train'].get('log_dir')
+# train_ds = get_fake_autoencoder_dataset(100, input_shape, batch_size=batch_size, repeat=False, interval=(0, 1))
+# val_ds = get_fake_autoencoder_dataset(100, input_shape, batch_size=batch_size, repeat=False, interval=(0, 1))
 
 
+encoder = build_encoder(input_shape=input_shape, output_shape=latent_size, filters=filters, kernel_size=kernel_size, pool_size=pool_size, batch_normalization=batch_normalization, name='my_encoder')
+decoder = build_decoder(input_shape=latent_size, output_shape=input_shape, filters=tuple(reversed(list(filters))), kernel_size=kernel_size, batch_normalization=batch_normalization, name='my_decoder')
 
-# mnist = tf.keras.datasets.mnist
-#
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# print('labels')
-# print(y_train)
-#
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(input_shape=(28, 28)),
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dropout(0.2),
-#     tf.keras.layers.Dense(10, activation='softmax')
-# ])
-#
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-#
-# # model.fit(x_train, y_train, epochs=5)
-# # model.evaluate(x_test, y_test, verbose=2)
+encoder.summary()
+decoder.summary()
+
+inputs = tf.keras.Input(shape=input_shape)
+x = encoder(inputs)
+outputs = decoder(x)
+autoencoder = tf.keras.models.Model(inputs, outputs)
+
+autoencoder.summary()
+
+autoencoder.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss=tf.keras.losses.MeanSquaredError(), metrics=[tf.keras.metrics.MeanSquaredError()])
+
+# print(train_ds)
+# sys.exit()
+autoencoder.fit(train_ds, epochs=10, steps_per_epoch=100, verbose=1, validation_data=val_ds, callbacks=[tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq=10)])
+
+
