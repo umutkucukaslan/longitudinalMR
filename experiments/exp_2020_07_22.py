6053b2fcd2389212875f1bd6e1569a65f747ce21
umutkucukaslan <umutkucukaslan@hotmail.com>
2020-07-23 12:08:41 +0300

    longitudinal training basic autoencoder, ssim metric, latent vector manipulation

--- experiments/exp_2020_07_22.py
+++ experiments/exp_2020_07_22.py
@@ -24,7 +24,7 @@ Loss: L2 loss
 """
 
 
-RUNTIME = "none"  # cloud, colab or none
+RUNTIME = "colab"  # cloud, colab or none
 USE_TPU = False
 RESTORE_FROM_CHECKPOINT = True
 EXPERIMENT_NAME = os.path.splitext(os.path.basename(__file__))[0]
