f93f3f27594756608e132b6e80ea500d73a93fe6
umutkucukaslan <umutkucukaslan@hotmail.com>
2020-12-27 23:26:20 +0300

    train on colab

--- reference_papers/glow/train_with_discriminator.py
+++ reference_papers/glow/train_with_discriminator.py
@@ -239,6 +239,8 @@ def train(
                 D_G.append(output.mean().item())
                 errs.append(err_fake)
             err = sum(errs) / 6.0
+            D_x = sum(D_x) / 3.0
+            D_G = sum(D_G) / 3.0
             discriminator_optimizer.step()
 
             # logdet = (logdet1.mean() + logdet2.mean()) / 2
@@ -257,7 +259,9 @@ def train(
             # pbar.set_description(
             #     f"Loss: {loss.item():.5f}; logP: {log_p.item():.5f}; logdet: {log_det.item():.5f}; pair_loss: {pair_loss.item()}; lr: {warmup_lr:.7f}"
             # )
-            pbar.set_description(f"Disc loss: {err.item()}")
+            pbar.set_description(
+                f"Disc loss: {err.item()}; D_x: {D_x.item()}; D_G: {D_G.item()}"
+            )
 
             if i % 100 == 0:
                 with torch.no_grad():
