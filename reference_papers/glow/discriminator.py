b4404185216af720162bcf2baee3f6c08812f431
umutkucukaslan <umutkucukaslan@hotmail.com>
2020-12-27 15:40:13 +0300

    push to colab fast

--- reference_papers/glow/discriminator.py
+++ reference_papers/glow/discriminator.py
@@ -87,6 +87,7 @@ if __name__ == "__main__":
     img_tensor = img_tensor.to(device)
     print("image tensor to devicee")
     print("i used tokyo command")
+    print("i used tokyo command")
     d_single = d_single.to(device)
     outputs = d_single(img_tensor.float())
     # outputs = d(img_tensor.float())
